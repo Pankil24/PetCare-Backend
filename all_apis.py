4f992bbdf72b67974b0f711d22c22084e615d1d1
Pankil24 <pankilpna@gmail.com>
2024-03-01 02:25:55 +0530

    New User count api added

--- all_apis.py
+++ all_apis.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import mysql.connector
 from datetime import date, datetime
 from flask_cors import CORS
+import math
+import json
+
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +20,7 @@ def register():
     try:
 
         data = request.get_json()
+        today = datetime.today()
 
         username = data["userName"]
         email = data["email"]
@@ -26,6 +30,7 @@ def register():
         gender = data["gender"]
         user_type = data["userType"]
         password = data["password"]
+        service_date = today.strftime("%Y-%m-%d %I:%M:%S %p")
 
         mycursor.execute("SELECT * FROM USERS where username = %s", (username,))
         user_exist = mycursor.fetchall()
@@ -35,7 +40,7 @@ def register():
             return jsonify("User name exist"), 201
         else:
             mycursor.execute(
-                "INSERT INTO users (username,password ,email, address, phone_number, age, gender, user_type) VALUES (%s, %s,%s, %s, %s, %s, %s, %s)",
+                "INSERT INTO users (username,password ,email, address, phone_number, age, gender, user_type,service_date) VALUES (%s, %s,%s, %s, %s, %s, %s, %s,%s)",
                 (
                     username,
                     password,
@@ -45,6 +50,7 @@ def register():
                     age,
                     gender,
                     user_type,
+                    service_date,
                 ),
             )
             conn.commit()
@@ -58,7 +64,20 @@ def register():
 @app.route("/users", methods=["GET"])
 def users():
     try:
-        mycursor.execute("select * from users")
+        # Get pagination parameters from query string
+        page = int(request.args.get("page", 1))  # Default to page 1 if not provided
+        page_size = int(
+            request.args.get("pageSize", 10)
+        )  # Default page size to 10 if not provided
+
+        # Calculate offset for pagination
+        offset = (page - 1) * page_size
+
+        mycursor.execute("SELECT COUNT(*) FROM users")
+        total_records = mycursor.fetchone()[0]
+        total_pages = (total_records + page_size - 1) // page_size
+
+        mycursor.execute("SELECT * FROM users LIMIT %s OFFSET %s", (page_size, offset))
         user_data = mycursor.fetchall()
 
         user_list = [
@@ -66,7 +85,15 @@ def users():
             for row in user_data
         ]
 
-        return jsonify(user_list), 200
+        response = {
+            "totalPages": total_pages,
+            "currentPage": page,
+            "pageSize": page_size,
+            "totalRecords": total_records,
+            "data": user_list,
+        }
+
+        return jsonify(response), 200
     except Exception as e:
         conn.rollback()
         return jsonify(f"Error: {str(e)}"), 500
@@ -164,23 +191,55 @@ def service():
         return jsonify(f"Error: {str(e)}"), 500
 
 
-@app.route("/serviceDetails")
+@app.route("/serviceDetails", methods=["GET"])
 def service_details():
-
     try:
+        # Get pagination parameters from query string
+        page = int(
+            request.args.get("pageIndex", 1)
+        )  # Default to page 1 if not provided
+        page_size = int(
+            request.args.get("pageSize", 10)
+        )  # Default page size to 10 if not provided
         userName = request.args.get("userId")
 
+        # Calculate offset for pagination
+        offset = (page - 1) * page_size
+
         if userName:
-            mycursor.execute("SELECT * FROM service WHERE username = %s ", (userName,))
+            mycursor.execute(
+                "SELECT COUNT(*) FROM service WHERE username = %s ", (userName,)
+            )
+            total_records = mycursor.fetchone()[0]
+            total_pages = math.ceil(total_records / page_size)
+
+            mycursor.execute(
+                "SELECT * FROM service WHERE username = %s LIMIT %s OFFSET %s",
+                (userName, page_size, offset),
+            )
         else:
-            mycursor.execute("SELECT * FROM service")
+            mycursor.execute("SELECT COUNT(*) FROM service")
+            total_records = mycursor.fetchone()[0]
+            total_pages = math.ceil(total_records / page_size)
+
+            mycursor.execute(
+                "SELECT * FROM service LIMIT %s OFFSET %s", (page_size, offset)
+            )
 
         user_service_details = [
             {mycursor.description[i][0]: value for i, value in enumerate(row)}
             for row in mycursor.fetchall()
         ]
 
-        return jsonify(user_service_details), 200
+        response = {
+            "totalPages": total_pages,
+            "currentPage": page,
+            "pageSize": page_size,
+            "totalRecords": total_records,
+            "data": user_service_details,
+        }
+
+        return jsonify(response), 200
 
     except Exception as e:
         conn.rollback()
@@ -212,11 +271,92 @@ def add_dog():
         return jsonify(f"Error: {str(e)}"), 500
 
 
+@app.route("/dogData", methods=["GET"])
+def get_dog_data():
+    try:
+        userName = request.args.get("username")
+
+        mycursor.execute("select * from dogs where username = %s", (userName,))
+        user_data = mycursor.fetchall()
+
+        print("USER Data =>", user_data)
+
+        user_list = [
+            {mycursor.description[i][0]: value for i, value in enumerate(row)}
+            for row in user_data
+        ]
+
+        return user_list, 200
+    except Exception as e:
+        conn.rollback()
+        return jsonify(f"Error: {str(e)}"), 500
+
+
+@app.route("/getUserCount", methods=["GET"])
+def get_user_count():
+    try:
+        mycursor.execute("SELECT * FROM users")
+        users_data = mycursor.fetchall()
+
+        # Filtering data to include only records with service_date month as 01 (January)
+        # Filtering data to include only records with service_date month as 01 (January)
+        new_data = []
+        for i in range(1, 13):
+            if len("0" + str(i)) == 3:
+                count = len(
+                    [
+                        data
+                        for data in users_data
+                        if data[9][5:7] == ("0" + str(i)).lstrip("0")
+                    ]
+                )
+            else:
+                count = len(
+                    [data for data in users_data if data[9][5:7] == ("0" + str(i))]
+                )
+            new_data.append(count)
+
+        print("New Data ==>", new_data)
+
+        months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ]
+
+        # Create a dictionary with month names as keys and counts as values
+        result = [
+            {"month": month, "user": count} for month, count in zip(months, new_data)
+        ]
+
+        # Returning all columns' data for the filtered records
+        return jsonify(result), 200
+    except Exception as e:
+        conn.rollback()
+        return jsonify(f"Error: {str(e)}"), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
 
 """
+
+all import pip command 
+
+pip install Flask
+pip install mysql-connector-python
+pip install flask_cors
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -226,7 +366,8 @@ CREATE TABLE users (
     phone_number VARCHAR(15) NOT NULL,
     age INT NOT NULL,
     gender ENUM('male', 'female', 'other') NOT NULL,
-    user_type ENUM('user','care_taker' ,'admin') NOT NULL
+    user_type ENUM('user','care_taker' ,'admin') NOT NULL,
+    service_date VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE service (
